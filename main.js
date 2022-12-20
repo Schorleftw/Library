826aa071f7bf2bfd17592f316493d33509239e71
bjoern.wasserfuhr <bjoern.wasserfuhr@telekom.de>
2022-12-20 15:23:10 +0100

    created function for displaying objects on the DOM

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+const addBook = document.querySelector(".addBook");
+const cards = document.querySelector(".grid");
+
 let myLibrary = [];
 
 
@@ -18,8 +21,43 @@ function addBookToLibrary(title, author, pages, read) {
     return myLibrary.push(new Book(title, author, pages, read));
 }
 
+function addToDom() {
+    for (let i = 0; i <= myLibrary.length - 1; i++) {
+        //für jedes Objekt im Array eine Card erstellen und die Klasse "card" adden
+        const newCard = document.createElement("div");
+        newCard.classList.add("card");
+
+        // Erstellen der p Elemente
+        const title = document.createElement("p");
+        title.innerText = myLibrary[i].title
+
+        const author = document.createElement("p");
+        author.innerText = myLibrary[i].author
+
+        const pages = document.createElement("p");
+        pages.innerText = myLibrary[i].pages
+
+        // Erstellen der Buttons
+        const readButton = document.createElement("button");
+        readButton.classList.add("read");
+        readButton.innerText = "Not read";
+
+        const deleteButton = document.createElement("button");
+        deleteButton.innerText = "Remove";
+
+        // Append the Div to the grid and the Elements to the Card
+        cards.appendChild(newCard);
+        newCard.append(title, author, pages, readButton, deleteButton);
+    }
+}
+
 // example user input
 addBookToLibrary("yikes", "Björn", "300", "not read yet");
 addBookToLibrary("The Hobbit", "J.R.R Tolkin", "295", "not read yet");
 
+addToDom();
+
+//addBook.addEventListener("click", addToDom);
+
 // console.log(myLibrary[0].info());
+// console.log(myLibrary[0].title);
