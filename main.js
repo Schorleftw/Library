dd1452159b854418fc51a9a63d064e337d8ad5eb
bjoern.wasserfuhr <bjoern.wasserfuhr@telekom.de>
2022-12-22 18:51:20 +0100

    submit functionality

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const addBook = document.querySelector(".addBook");
 const cards = document.querySelector(".grid");
+const submitButton = document.querySelector(".submit");
 
 let myLibrary = [];
 
@@ -52,15 +53,44 @@ function addToDom() {
 }
 
 function openForm() {
+    // activate overlay
     document.querySelector(".overlay").style.display = "block";
+    // activate popup
     document.querySelector(".form-popup").style.display = "block";
 }
+
+function closeForm() {
+    document.querySelector(".overlay").style.display = "none";
+    document.querySelector(".form-popup").style.display = "none";
+}
+
+submitButton.addEventListener("click", function submit(e) {
+    // When submit button is clicked grab the user input and pass it into the array and call addToDom()
+    // Prevent default behavior of submit button
+    e.preventDefault();
+    const title = document.querySelector(".getTitle").value;
+    const author = document.querySelector(".getAuthor").value;
+    const pages = document.querySelector(".getPages").value;
+    let read = "";
+
+    if (document.querySelector(".checkbox").checked) {
+        read = "read"
+    }
+    else {
+        read = "not read yet"
+    }
+
+    addBookToLibrary(title, author, pages, read);
+    addToDom();
+    closeForm();
+});
+
 
 addBook.addEventListener("click", openForm);
 
 // example user input
-addBookToLibrary("yikes", "Björn", "300", "not read yet");
-addBookToLibrary("The Hobbit", "J.R.R Tolkin", "295", "not read yet");
+// addBookToLibrary("yikes", "Björn", "300", "not read yet");
+// addBookToLibrary("The Hobbit", "J.R.R Tolkin", "295", "not read yet");
 
 addToDom();
 
